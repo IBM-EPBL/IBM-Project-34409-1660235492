b51db8093a06e7df77f9c9cb05af7655aaa3a9fd
cube <110179449+cube786@users.noreply.github.com>
2022-11-16 05:03:13 -0800

    Create publish data.py

---
+++ Project Design and Planning/Project Development Phase/Sprint 2/publish data.py
@@ -0,0 +1,39 @@
+import wiotp.sdk.device # python -m pip install wiotp
+import time
+
+# IMPORT SECTION ENDS
+# -----------------------------------------------
+# API CONFIG SECTION STARTS
+
+myConfig = {
+    "identity" : {
+        "orgId" : "gsqz5f",
+        "typeId" : "NANDY",
+        "deviceId" : "12345"
+    },
+    "auth" : {
+        "token" : "9876543210"
+    }
+}
+
+# API CONFIG SECTION ENDS
+# -----------------------------------------------
+# FUNCTIONS SECTION STARTS
+
+def myCommandCallback(cmd):
+    print("recieved cmd : ",cmd)
+
+
+def logData2Cloud(location,temperature,visibility):
+    client = wiotp.sdk.device.DeviceClient(config=myConfig,logHandlers=None)
+    client.connect()
+    client.publishEvent(eventId="status",msgFormat="json",data={
+        "temperature" : temperature,
+        "visibility" : visibility,
+        "location" : location
+    },qos=0,onPublish=None)
+    client.commandCallback = myCommandCallback
+    client.disconnect()
+    time.sleep(500)
+
+# FUNCTIONS SECTION ENDS
